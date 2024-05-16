e3784644d8891f129fc7ae7b58bce2f6ea7ad3f4
yairAll <yairshainberger@gmail.com>
2024-05-16 12:19:50 +0300

    finishLOGin

--- Program.cs
+++ Program.cs
@@ -2,21 +2,17 @@
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Net;
 using Microsoft.EntityFrameworkCore;
+using UUIDNext;
 using static Project.Utils;
 
 namespace Project;
 
 //mandatory changes
 class Program
-{static void Main()
-    {
-        Dictionary<string, int> votes = new Dictionary<string, int>();
-        votes.Add("ferrari", 0);
-        votes.Add("porsche", 0);
-        votes.Add("lamborghini", 0);
-        votes.Add("mclaren", 0);
-        votes.Add("bugatti", 0);
-        votes.Add("other",0);
+{
+  static void Main()
+  {
+    
     /*───────────────────────────╮
     │ Creating the server object │
     ╰───────────────────────────*/
@@ -66,7 +62,7 @@ class Program
         Console.ForegroundColor = ConsoleColor.Red;
         Console.WriteLine(e);
         Console.ResetColor();
-      }
+      } 
 
       /*───────────────────────────────────╮
       │ Sending the response to the client │
@@ -75,29 +71,54 @@ class Program
     }
   }
 
-  static void HandleRequests(HttpListenerContext serverContext, DatabaseContext databaseContext)
+  static void HandleRequests(HttpListenerContext serverContext, DatabaseContext database)
   {
     var request = serverContext.Request;
     var response = serverContext.Response;
 
     string absPath = request.Url!.AbsolutePath;
-  
-  if (absPath == "/vote") {
-                string company = GetBody(request);
 
-                votes[company]++;
-            }
-            else if (absPath == "/getVotes") {
-                string votesJson = JsonSerializer.Serialize(votes);
-                byte[] votesBytes = Encoding.UTF8.GetBytes(votesJson);
-                response.OutputStream.Write(votesBytes);
-            }
-  
+     if (absPath == "/signup"){
+      (string username , string password) = request.GetBody<(string,string)>();
 
+      var userId = Uuid.NewDatabaseFriendly(UUIDNext.Database.SQLite).ToString();
+
+      var user = new User(userId , username,password);
+      database.Users.Add(user);
+
+      response.Write(userId);
+
+      
+     }
+
+     else if ( absPath == "/login")
+     {
+      (string username, string password) = request.GetBody<(string, string)>();
+
+      User user = database.Users.First(
+        u => u.Username == username && u.Password == password
+      )!;
+
+      response.Write(user.Id);
+    }
   }
+
 }
 
 class DatabaseContext : DbContextWrapper
 {
+  public DbSet<User> Users {get; set;}
+  // public DbSet<Favorite> favorites { get; set; }
+  
   public DatabaseContext() : base("Database") { }
 }
+
+
+class User(string id, string username, string password)
+{
+  [Key]
+  public string Id { get; set; } = id ;
+  public string Username { get; set; } = username ;
+  public string Password { get; set; } = password ;
+}
+
